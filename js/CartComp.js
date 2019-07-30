fa419f79d238bf369090c4b80e42c411e6faff16
andreycoder <Sdamvse$vbank#1>
2019-07-30 16:41:32 +0300

    Прикрутил Vue. Товары и корзина генерятся, но на странице корзины пока нет синхронизации между компонентами

---
+++ js/CartComp.js
@@ -0,0 +1,194 @@
+Vue.component('cart', {
+    props:['dropCart'],
+    data(){
+        return {
+            cartUrl: '/getCart.json',
+            cart: {
+                "amount": 0,
+                "countGoods": 0,
+                "contents":[]
+            },
+        }
+    },
+    computed:{
+      countGoods(){
+          this.cart.countGoods =  this.cart.contents.reduce((sum,carrent) => {return sum+carrent.quantity} , 0 );
+          return this.cart.countGoods;
+      },
+        amount(){
+            this.cart.amount =  this.cart.contents.reduce((sum,carrent) => {return sum+carrent.price*carrent.quantity} , 0 );
+            return this.cart.amount;
+        }
+    },
+    methods: {
+        addProduct(product){
+            this.$parent.getJson(`${API}/addToCart.json`)
+                .then(data => {
+                    if(data.result === 1){
+                        let find = this.cart.contents.find(el => el.id_product === product.id_product);
+                        if(find){
+                            find.quantity++;
+                        } else {
+                            let prod = Object.assign({quantity: 1}, product);
+                            this.cart.contents.push(prod)
+                        }
+                    } else {
+                        alert('Error');
+                    }
+                })
+        },
+        remove(item) {
+            this.$parent.getJson(`${API}/deleteFromCart.json`)
+                .then(data => {
+                    if(data.result === 1) {
+                        if(item.quantity>1){
+                            item.quantity--;
+                        } else {
+                            this.cart.contents.splice(this.cart.contents.indexOf(item), 1)
+                        }
+                    }
+                })
+        },
+    },
+    mounted(){
+        this.$parent.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                this.cart.amount = data.amount;
+                this.cart.countGoods = data.countGoods;
+                for(let el of data.contents){
+                    this.cart.contents.push(el);
+                }
+            });
+    },
+    template: `
+                <div class="header__right-cart" v-if="dropCart"><a href="shoping-cart.html"><img src="img/cart.svg" alt="cart"></a>
+                    <div class="ellipse-cart">{{countGoods}}</div>
+                    <div class="drop drop-cart">
+                        <div class="drop-box-triangle triangle-cart"></div>
+                        <div class="drop-box">
+                        <p class="drop-box_empty" v-if="!cart.contents.length">Cart is empty</p>
+                        <drop-cart-item class="cart-item"
+                        v-for="item of cart.contents"
+                        :key="item.id_product"
+                        :cart-item="item"
+                        @remove="remove">
+                        </drop-cart-item>
+
+
+                            <div class="drop-cart__total" v-if="cart.contents.length">
+                                <p>TOTAL</p>
+                                <p>$ {{amount.toFixed(2)}}</p>
+                            </div>
+                            <a href="checkout.html" class="button drop-cart_button" v-if="cart.contents.length">Checkout</a>
+                            <a href="shoping-cart.html" class="button drop-cart_button" v-if="cart.contents.length">Go to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="cart" v-else>
+    <div class="container">
+    <div class="cart__grow">
+    <div class="cart__grow-box">
+    <span class="cart__grow-col cart__grow-col-head">Product Details</span>
+<span class="cart__grow-col cart__grow-col-head">unite Price</span>
+<span class="cart__grow-col cart__grow-col-head">Quantity</span>
+    <span class="cart__grow-col cart__grow-col-head">shipping</span>
+    <span class="cart__grow-col cart__grow-col-head">Subtotal</span>
+    <span class="cart__grow-col cart__grow-col-head">ACTION</span>
+    </div>
+    <cart-item class="cart-item"
+                        v-for="item of cart.contents"
+                        :key="item.id_product"
+                        :cart-item="item"
+                        @remove="remove">
+                        </cart-item>
+
+            </div>
+    <div class="cart__buttons">
+    <a href="#" class="button cart__form-button"> cLEAR SHOPPING CART</a>
+<a href="#" class="button cart__form-button"> cONTINUE sHOPPING</a>
+</div>
+<form action="#" class="cart__form">
+    <fieldset class="cart__form-col">
+    <legend class="cart__form-legend">Shipping Adress</legend>
+<select name="country" id="country" class="cart__form-input">
+    <option value="bangladesh">bangladesh</option>
+    <option value="bangladesh">bangladesh</option>
+    <option value="bangladesh">bangladesh</option>
+    <option value="bangladesh">bangladesh</option>
+    </select>
+    <input type="text" placeholder="State" class="cart__form-input">
+    <input type="text" placeholder="Postcode / Zip" class="cart__form-input">
+    <div class="button cart__form-button">get a quote</div>
+</fieldset>
+<fieldset class="cart__form-col">
+    <legend class="cart__form-legend">coupon discount</legend>
+<p class="cart__form-descr">
+    Enter your coupon code if you have one
+</p>
+<input type="text" placeholder="State" class="cart__form-input">
+    <a class="button cart__form-button">Apply coupon</a>
+</fieldset>
+<fieldset class="cart__form-col">
+    <div class="cart__total">
+    <p class="cart__form-subtotal">Sub total <span class="cart__form-subtotal-span">{{amount}}</span></p>
+<p class="cart__form-grandtotal">GRAND TOTAL <span
+class="cart__form-grandtotal-span pink">{{amount}}</span></p>
+<div class="button cart__form-button-pink">proceed to checkout</div>
+</div>
+
+</fieldset>
+</form>
+
+</div>
+</div>
+`
+});
+
+Vue.component('drop-cart-item', {
+    props: ['cartItem'],
+    template: `
+                            <div class="drop-cart__item">
+                                <a href="single-product.html" class="drop-cart__item-image"><img :src="cartItem.image"
+                                                                                                 alt="cartItem.product_name"></a>
+                                <div class="drop-cart__item-description">
+                                    <h3 class="drop-cart__item-title">{{cartItem.product_name}}</h3>
+                                    <p class="drop-cart__item-star"><i class="fa fa-star" aria-hidden="true"></i><i
+                                            class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star"
+                                                                                         aria-hidden="true"></i><i
+                                            class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star-half-o"
+                                                                                         aria-hidden="true"></i></p>
+                                    <p class="drop-cart__item-price">{{cartItem.quantity}}<span class="drop-cart__item-price-span">x</span>
+                                        {{cartItem.price}}</p>
+                                </div>
+                                <span class="drop-cart__item-del" @click="$emit('remove', cartItem)"><i class="fa fa-times-circle"
+                                                                     aria-hidden="true"></i></span>
+                            </div>
+
+
+    `
+});
+
+Vue.component('cart-item', {
+    props: ['cartItem'],
+    template: `
+
+                            <div class="cart__grow-box">
+                                <div class="cart__grow-col cart__grow-col-first ">
+                                <img class="cart__grow-image" :src="cartItem.image" alt="cartItem.product_name">
+                                <div class="cart__grow-description">
+                                <a href="single-product.html" class="cart__grow-description-title">{{cartItem.product_name}}</a>
+                                <div class="cart__grow-description-param">
+                                <p>Color: <span class="cart__grow-description-param-span">{{cartItem.color}}</span></p>
+                                <p>Size: <span class="cart__grow-description-param-span">{{cartItem.size}}</span></p>
+                                </div>
+                                </div>
+                                </div>
+                                <p class="cart__grow-col">{{cartItem.price}}</p>
+                                <p class="cart__grow-col"><input class="cart__grow-col-number" type="number" min="1" max="10" v-model="cartItem.quantity"></p>
+                                <p class="cart__grow-col">free</p>
+                                <p class="cart__grow-col">{{cartItem.quantity*cartItem.price}}</p>
+                                <p class="cart__grow-col cart__grow-col-del" @click="$emit('remove', cartItem)"><i class="fa fa-times-circle" aria-hidden="true"></i>
+                                </p>
+                            </div>
+    `
+});
---
+++ js/ProductComp.js
@@ -0,0 +1,56 @@
+Vue.component('products', {
+    props:['count'],
+    data(){
+        return {
+            catalogUrl: '/catalogData.json',
+            products: [],
+            filtered: [],
+
+        }
+    },
+    methods: {
+        countRender(){
+            if (this.count){
+                return this.count
+            }else {
+                return this.products.length
+            }
+
+        }
+    },
+    mounted(){
+        this.$parent.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for(let el of data){
+                    this.products.push(el);
+                    this.filtered.push(el);
+                }
+            });
+
+    },
+    template: `
+        <div class="product-flex">
+            <product v-for="item of filtered" v-if="filtered.indexOf(item) < countRender()" :key="item.id_product" :product="item"></product>
+        </div>
+    `
+});
+Vue.component('product', {
+    props: ['product'],
+    data() {
+        return {
+           cartAPI: this.$root.$refs.cart,
+        };
+    },
+
+    template: `
+                <div class="product-item">
+                    <a href="single-product.html"><img class="product-item__image" :src="product.image"
+                                                       alt="product1"></a>
+                    <p class="product-item__addtocart" @click="cartAPI.addProduct(product)"><img src="img/cart-white.svg" alt="cart-white">Add to Cart</p>
+                    <div class="product-item__text">
+                        <a href="single-product.html" class="product-item__title">{{product.product_name}}</a>
+                        <p class="product-item__price pink">$ {{product.price.toFixed(2)}}</p>
+                    </div>
+                </div>
+    `
+});
